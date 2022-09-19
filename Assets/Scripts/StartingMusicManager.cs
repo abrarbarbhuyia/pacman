7c2428b38cf22b2686262c43f4e13ed6c5778454
Abrar Barbhuyia <abrarbarbhuyia@192-168-1-17.tpgi.com.au>
2022-09-19 14:02:05 +1000

    added audio source for starting and background music + script

---
+++ Assets/Scripts/StartingMusicManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartingMusicManager : MonoBehaviour
+{
+    AudioSource introMusicSource;
+    AudioSource normalStateMusicSource;
+    public AudioClip normalStateMusic;
+    bool isPlayingNormalState;
+    // Start is called before the first frame update
+    void Start()
+    {
+        introMusicSource = GetComponent<AudioSource>();
+        normalStateMusicSource = gameObject.AddComponent<AudioSource>();
+        isPlayingNormalState = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!introMusicSource.isPlaying && !isPlayingNormalState) {
+            normalStateMusicSource.clip = normalStateMusic;
+            normalStateMusicSource.Play();
+            normalStateMusicSource.loop = true;
+            isPlayingNormalState = true;
+        }
+    }
+}
